20396fe4c1ef5b7cdde31e670ce8b11283921252
xmtfs <49993334+xmtfs@users.noreply.github.com>
2019-10-04 19:11:58 +0200

    Delete money.js

--- money.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    /* This code is executed after the DOM has been completely loaded */
-
-    $('#download-button a').click(function(){
-        var countSpan = $(this).closest('li').find('.download-count');
-        countSpan.text( parseInt(countSpan.text(), 10) + 1);
-    }); 
-})
