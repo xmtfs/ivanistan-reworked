e5a2bb8fa79f144dcd1eb280c001d8db2e2103e1
xmtfs <49993334+xmtfs@users.noreply.github.com>
2019-10-04 19:13:14 +0200

    Add files via upload

---
+++ idex.js
@@ -0,0 +1,27 @@
+//THIS CODE IS OWNED BY THE REPUBLIC OF IVANISTAN USE IS ALLOWED BUT COPYING IS NOT
+function addLeadingZero(n) {
+    if (n < 10) {
+        n = "0" + n;
+    }
+    return n;
+}
+
+function windTheClock(timeZoneOffset)
+{
+    var d = new Date();
+    d.setHours(d.getUTCHours() + timeZoneOffset); 
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    h = addLeadingZero(h);
+    m = addLeadingZero(m);
+    s = addLeadingZero(s);
+    document.all["time"].innerHTML = h + ":" + m + ":" + s;
+    setTimeout(function(){ windTheClock(timeZoneOffset) }, 1000);
+}
+
+window.onload = function() {
+    windTheClock(2);
+}
+
+
