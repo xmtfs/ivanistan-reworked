133890f18d1339d7e2340da00397401bb95aec50
xmtfs <nesalomiviistrazivac@gmail.com>
2019-11-22 21:41:46 +0100

    modified:   constitution.html 	modified:   diplomacy.html 	modified:   government.html 	modified:   government_agencies.html 	modified:   index.html 	modified:   national_bank.html 	modified:   news.html 	renamed:    index.css -> resources/css/index.css 	renamed:    Constitution of Ivanistan 23_9_2018.docx -> resources/doc/Constitution of Ivanistan 23_9_2018.docx 	renamed:    favicon.png -> resources/images/favicon.png 	renamed:    flag.jpg -> resources/images/flag.jpg 	renamed:    president.jpg -> resources/images/president.jpg 	renamed:    index.js -> resources/js/index.js 	modified:   the_presedent.html

---
+++ resources/js/index.js
@@ -0,0 +1,29 @@
+//THIS CODE IS OWNED BY THE REPUBLIC OF IVANISTAN USE IS ALLOWED BUT COPYING IS NOT
+function addLeadingZero(n) {
+    if (n < 10) {
+        n = "0" + n;
+    }
+    return n;
+}
+
+function windTheClock()
+{
+    var d = new Date();
+    var timeZoneOffset = -d.getTimezoneOffset()/60;
+    d.setHours(d.getUTCHours() + timeZoneOffset); 
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    h = addLeadingZero(h);
+    m = addLeadingZero(m);
+    s = addLeadingZero(s);
+    document.all["time"].innerHTML = h + ":" + m + ":" + s;
+    setTimeout(function(){ windTheClock(timeZoneOffset) }, 1000);
+}
+
+window.onload = function() {
+    windTheClock();
+}
+
+
+
